dbd11fe3baa77bd7c3e13e88aad93bf3460bd2b9
Василий Игнатов <vasily.ignatov@intermobility.ru>
2025-09-07 04:55:45 +0300

    🔧 Исправлена ошибка зависимостей: добавлен DatabaseModule в AkoolModule
    - AkoolService и AkoolWebhookController теперь могут использовать PG_POOL
    - Исправлена ошибка UnknownDependenciesException
    - Приложение должно запуститься без ошибок

--- src/akool/akool.module.ts
+++ src/akool/akool.module.ts
@@ -4,9 +4,10 @@ import { AkoolService } from './akool.service';
 import { AkoolController } from './akool.controller';
 import { AkoolWebhookController } from './akool-webhook.controller';
 import { ElevenLabsModule } from '../elevenlabs/elevenlabs.module';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
-  imports: [ConfigModule, ElevenLabsModule],
+  imports: [ConfigModule, ElevenLabsModule, DatabaseModule],
   providers: [AkoolService],
   controllers: [AkoolController, AkoolWebhookController],
   exports: [AkoolService],
